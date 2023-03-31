60e4dd5cda656db9ed7357353624e4effa003368
Manas Tiwari <tiwarimanas27@gmail.com>
2023-03-31 14:23:04 -0400

    deploying

--- src/App.tsx
+++ src/App.tsx
@@ -13,9 +13,9 @@ function App() {
         <Navbar />
         <Container className="mb-4">
           <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/store" element={<Store />} />
-            <Route path="/about" element={<About />} />
+            <Route path="/" element={<Store />} />
+            {/* <Route path="/store" element={<Store />} />
+            <Route path="/about" element={<About />} /> */}
           </Routes>
         </Container>
       </ShoppingCartProvider>
